59476428048b2316cafe66adf46d703129ba369e
jeffersonbalde <jeffersonbalde8@gmail.com>
2023-07-07 10:17:47 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <div className="">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!s
+      </h1>
+      {/* <button className="btn btn-info">Button</button> */}
+      {/* <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">Responsive</button> */}
+    </div>
+  )
+}
