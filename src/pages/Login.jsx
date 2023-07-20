104b051be5cfd06bcc5468072786657269ac23d4
jeffersonbalde <jeffersonbalde8@gmail.com>
2023-07-20 15:16:28 +0800

    updating hero section

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -29,9 +29,9 @@ const Login = () => {
     <div className="bg-[#E8E8E8] w-screen h-screen font-inter">
       <div className="flex flex-col justify-center items-center w-full h-full">
         <div className="flex flex-col items-center justify-center">
-          <h1 className="text-[#000000] text-[50px] font-bold upper sm:text-8xl">BeshyConvo</h1>
-          <p className="text-[#000000] text-base mt-3 w-[330px] text-center sm:text-2xl sm:w-[600px]">Join the BeshyConvo, 
-            meet new Beshify users, and build connections within a shared chat room
+          <h1 className="text-[#000000] text-[50px] font-bold upper sm:text-8xl">WebChat</h1>
+          <p className="text-[#000000] text-base mt-3 w-[330px] text-center sm:text-2xl sm:w-[600px]">Join the conversation, meet new people, 
+            and make connections in one shared room
           </p>
           <div 
             className=" mt-7 border border-[#000000] justify-center py-2 px-4 bg-[#ffffff] rounded-full flex flex-row items-center cursor-pointer"
